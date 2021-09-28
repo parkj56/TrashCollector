1b35396f5389a63cacfeacc1a8d97d3a2976e753
asnchz <asnchz27@yahoo.com>
2021-09-28 09:37:42 -0500

    employees models.py created

---
+++ trash_collector/trash_collector/local_settings.py
@@ -0,0 +1,19 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '-2#se2w2t^20h%7g6_6+(zztxlmz#99f*r(dgsri7@ip_8zov@'
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'trash_collector',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}
